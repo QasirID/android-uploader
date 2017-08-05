248986c35c5b4593942f4886974982864e2f2db9
Novan Adrian <novanadrian@gmail.com>
2017-08-05 11:10:28 +0700

    Use ANDROID_HOME as base path

--- apkrun.sh
+++ apkrun.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 # declare STRING variable
 #echo -e $(adb devices);
-ADB_SOURCE="/Users/hartviq/Library/Android/sdk/platform-tools/adb";
+
+if [ $ANDROID_HOME = "" ]; then
+    echo "Please set ANDROID_HOME first."
+    exit 0
+fi
+
+if [ $1 = "" ]; then
+    echo "Please define device name"
+    exit 0
+fi
+
+if [ $2 = "" ]; then
+    echo "Please define package name"
+    exit 0
+fi
+
+ADB_SOURCE="$ANDROID_HOME/platform-tools/adb"
 DEVICE_NAME=$1
-PACKAGE_NAME=$2;
+PACKAGE_NAME=$2
 
 ADBDEVICES=$($ADB_SOURCE -s $DEVICE_NAME  shell monkey -p $PACKAGE_NAME -c android.intent.category.LAUNCHER 1);
 echo $ADBDEVICES
